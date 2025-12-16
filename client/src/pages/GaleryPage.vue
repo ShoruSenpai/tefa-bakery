aeb6ef80b28832a8919e1f36579e4f956991bc81
ShoruSenpai <shorusenpai@gmail.com>
2025-12-16 20:11:15 +0700

    fixing aboutpage (corrupt)

--- client/src/pages/GaleryPage.vue
+++ client/src/pages/GaleryPage.vue
@@ -1,3 +1,18 @@
 <template>
-  <div></div>
+  <div class="font-[quicksand] text-primary">
+    <section>
+      <div class="h-full min-w-screen flex flex-col gap-24 container m-auto items-center">
+        <!-- heading -->
+        <div class="w-full flex flex-col gap-6 mt-36 items-center">
+          <h1 class="text-5xl font-semibold">Galeri Kegiatan</h1>
+          <p class="text-md font-medium">
+            Dokumentasi berbagai aktivitas siswa dalam proses produksi, pelatihan, dan promosi
+            produk di TEFA Bakery Polije.
+          </p>
+        </div>
+        <!-- main content -->
+        <div></div>
+      </div>
+    </section>
+  </div>
 </template>
